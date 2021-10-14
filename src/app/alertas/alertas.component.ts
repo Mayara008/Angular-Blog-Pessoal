ba04b70c121164782bbda4f217ab72122ca9f4d8
mayaramachado <mayara_b2k@hotmail.com>
2021-10-13 21:37:52 -0300

    Alertas reutilizaveis implemetados PARTE 01

---
+++ src/app/alertas/alertas.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-alertas',
+  templateUrl: './alertas.component.html',
+  styleUrls: ['./alertas.component.css']
+})
+export class AlertasComponent implements OnInit {
+ //Como importar uma variavel que está en outro .... 
+  @Input() message: string
+  @Input() tipo: string = 'success' //= Para receber o valor nessa váriavel
+
+  constructor(
+    public modal: BsModalRef
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onClose(){
+    this.modal.hide()
+  }
+
+}
